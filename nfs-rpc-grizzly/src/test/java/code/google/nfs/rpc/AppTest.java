08496a58e69e7959852871a4555ac8bf32ec8fdc
leeyazhou <bytesgo@163.com>
2025-09-04 10:32:33 +0800

    refact: remove module nfs-rpc-mina and nfs-rpc-netty

--- nfs-rpc-grizzly/src/test/java/code/google/nfs/rpc/AppTest.java
+++ nfs-rpc-grizzly/src/test/java/code/google/nfs/rpc/AppTest.java
@@ -1,33 +1,16 @@
 package code.google.nfs.rpc;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest extends TestCase {
-  /**
-   * Create the test case
-   *
-   * @param testName name of the test case
-   */
-  public AppTest(String testName) {
-    super(testName);
-  }
+public class AppTest {
 
-  /**
-   * @return the suite of tests being tested
-   */
-  public static Test suite() {
-    return new TestSuite(AppTest.class);
-  }
-
-  /**
-   * Rigourous Test :-)
-   */
-  public void testApp() {
-    assertTrue(true);
-  }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 }
